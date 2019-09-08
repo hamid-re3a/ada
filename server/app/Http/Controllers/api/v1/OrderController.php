84e02d67dd3ebac424e8b8fbae6e180f8436505f
hamid-re3a <hamidrezanoruzinejad@yahoo.com>
2019-09-08 12:44:54 +0430

    ser

--- server/app/Http/Controllers/api/v1/OrderController.php
+++ server/app/Http/Controllers/api/v1/OrderController.php
@@ -29,7 +29,7 @@ class OrderController extends AuthController
             'gender' => array('required', 'in:male,female'),
             'phone_number' => array('required'),
             'date' => array('required'),
-            'time' => array('required','number','max:20'),
+            'time_slot' => array('required','number','max:20'),
             'service_id' => array('required','exists:services'),
         );
 
@@ -37,90 +37,94 @@ class OrderController extends AuthController
         if ($validator->fails()) {
             return $this->respondBadRequest($validator->errors()->first());
         }
-        $input = $request->only('phone_number', 'gender', 'service_id','date','time');
-        $input['status'] = "waiting";
+        $obj = Order::where('date',$request->date)->where('time_slot',$request->time_slot)->first();
+        if(isset($obj) && isset($obj->id)){
+            return $this->respondBadRequest("این زمان قبلا رزرو شده است");
+        }
+        $input = $request->only('phone_number', 'gender', 'service_id','date','time_slot');
+        $input['status'] = "paid";
         $order = Order::create($input);
 
-        $service_obj = Service::where('id', $request->service_id)->first();
-        $price = str_replace(',','',$service_obj->cost);
+//        $service_obj = Service::where('id', $request->service_id)->first();
+//        $price = str_replace(',','',$service_obj->cost);
 
 
 
-        $response = $this->send($this->api, $price , $this->redirect, $order->id);
+//        $response = $this->send($this->api, $price , $this->redirect, $order->id);
 
 
-        $status = $response->status ?? null;
-        $trans_id = $response->transId ?? null;
+//        $status = $response->status ?? null;
+//        $trans_id = $response->transId ?? null;
+//
+//        if($status == 1 && $trans_id != null){
+//            $order->trans_id = $trans_id;
+//            $order->save();
 
-        if($status == 1 && $trans_id != null){
-            $order->trans_id = $trans_id;
-            $order->save();
-
-            $message = " درخواست شما با کد پیگیری $order->id با موفقیت ثبت شد، لطفا نسبت به پرداخت وجه اقدام فرمایید ";
+            $message = " درخواست شما با کد پیگیری $order->id با موفقیت ثبت شد ";
             $this->sendSMS($user->phone_number,$message );
 
-            return $this->respondSuccessfully('عملیات با موفقیت انجام شد',["url"=>"https://pay.ir/payment/gateway/$trans_id"]);
-        } else {
-            $order->delete();
-            return $this->respondCriticalError('درخواست شما با مشکل مواجه شد');
-        }
+            return $this->respondSuccessfully('عملیات با موفقیت انجام شد');
+//        } else {
+//            $order->delete();
+//            return $this->respondCriticalError('درخواست شما با مشکل مواجه شد');
+//        }
 
     }
 
-    public function verify_transaction_get(Request $request){
+//    public function verify_transaction_get(Request $request){
+//
+//        $rules = array(
+//            'trans_id' => array('required','exists:orders,trans_id'),
+//        );
+//
+//        $validator = Validator::make($request->all(), $rules);
+//
+//        if ($validator->fails()) {
+//            return $this->respondBadRequest('دوباره تلاش کنید');
+//        }
+//
+//
+//        $order_obj = Order::where('trans_id',$request->trans_id)->first();
+//
+//        $response = $this->verify($this->api, $request->trans_id);
+//        $order_obj->status = $response->status == 1 ? "paid" : "waiting";
+//        $order_obj->save();
+//        return $this->respondSuccessfully('پاسخ دریافت شد',$response);
+//    }
 
-        $rules = array(
-            'trans_id' => array('required','exists:orders,trans_id'),
-        );
-
-        $validator = Validator::make($request->all(), $rules);
-
-        if ($validator->fails()) {
-            return $this->respondBadRequest('دوباره تلاش کنید');
-        }
-
-
-        $order_obj = Order::where('trans_id',$request->trans_id)->first();
-
-        $response = $this->verify($this->api, $request->trans_id);
-        $order_obj->status = $response->status == 1 ? "paid" : "waiting";
-        $order_obj->save();
-        return $this->respondSuccessfully('پاسخ دریافت شد',$response);
-    }
-
-    public function verify_transaction($order_obj){
-
-        if($order_obj->status === 0 || $order_obj->status === 1){
-            return $this->respondSuccessfully('پاسخ دریافت شد',['status'=>$order_obj->status , 'transId'=>$order_obj->trans_id]);
-        }
-        $response = $this->verify($this->api, $order_obj->trans_id);
-        $order_obj->status = $response->status == 1 ? "paid" : "waiting";
-        $order_obj->save();
-        return $order_obj->status;
-    }
-    public function paid(Request $request){
-
-        $rules = array(
-            'factorNumber' => array('required','exists:orders,id'),
-            'status' => array('required','integer','max:1'),
-            'mobile' => array('integer'),
-            'cardNumber' => array('string'),
-            'message' => array('string'),
-        );
-
-        $validator = Validator::make($request->all(), $rules);
-
-        if ($validator->fails()) {
-            return $this->respondBadRequest('دوباره تلاش کنید');
-        }
-        $order = Order::find($request->factorNumber);
-
-        $status = $this->verify_transaction($order);
-        if( $status == "paid") {
-            $response = " رزرو  شما با شماره پیگیری $order->track_number   پرداخت شد. ";
-            $this->sendSMS($order->user->phone_number,$response );
-        }
-    }
+//    public function verify_transaction($order_obj){
+//
+//        if($order_obj->status === 0 || $order_obj->status === 1){
+//            return $this->respondSuccessfully('پاسخ دریافت شد',['status'=>$order_obj->status , 'transId'=>$order_obj->trans_id]);
+//        }
+//        $response = $this->verify($this->api, $order_obj->trans_id);
+//        $order_obj->status = $response->status == 1 ? "paid" : "waiting";
+//        $order_obj->save();
+//        return $order_obj->status;
+//    }
+//    public function paid(Request $request){
+//
+//        $rules = array(
+//            'factorNumber' => array('required','exists:orders,id'),
+//            'status' => array('required','integer','max:1'),
+//            'mobile' => array('integer'),
+//            'cardNumber' => array('string'),
+//            'message' => array('string'),
+//        );
+//
+//        $validator = Validator::make($request->all(), $rules);
+//
+//        if ($validator->fails()) {
+//            return $this->respondBadRequest('دوباره تلاش کنید');
+//        }
+//        $order = Order::find($request->factorNumber);
+//
+//        $status = $this->verify_transaction($order);
+//        if( $status == "paid") {
+//            $response = " رزرو  شما با شماره پیگیری $order->track_number   پرداخت شد. ";
+//            $this->sendSMS($order->user->phone_number,$response );
+//        }
+//    }
 
     /**
      * @param Request $request
@@ -138,27 +142,27 @@ class OrderController extends AuthController
         return Order::tomorrow()->all()->paginate(20);
     }
 
-    private function send($api, $amount, $redirect, $factorNumber=null) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://pay.ir/payment/send');
-        curl_setopt($ch, CURLOPT_POSTFIELDS,"api=$api&amount=$amount&redirect=$redirect&factorNumber=$factorNumber");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $res = curl_exec($ch);
-        curl_close($ch);
-        $result = (object) json_decode($res,true);
-        return $result;
-    }
-
-    private function verify($api, $transId) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://pay.ir/payment/verify');
-        curl_setopt($ch, CURLOPT_POSTFIELDS, "api=$api&transId=$transId");
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-        $res = curl_exec($ch);
-        curl_close($ch);
-        $result = (object) json_decode($res,true);
-        return $result;
-    }
+//    private function send($api, $amount, $redirect, $factorNumber=null) {
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, 'https://pay.ir/payment/send');
+//        curl_setopt($ch, CURLOPT_POSTFIELDS,"api=$api&amount=$amount&redirect=$redirect&factorNumber=$factorNumber");
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//        $res = curl_exec($ch);
+//        curl_close($ch);
+//        $result = (object) json_decode($res,true);
+//        return $result;
+//    }
+//
+//    private function verify($api, $transId) {
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_URL, 'https://pay.ir/payment/verify');
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, "api=$api&transId=$transId");
+//        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+//        $res = curl_exec($ch);
+//        curl_close($ch);
+//        $result = (object) json_decode($res,true);
+//        return $result;
+//    }
 }
